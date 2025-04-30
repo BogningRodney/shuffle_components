16e0b90285a513db9b1122ae9db3dc7256f8475d
Bogning Rodney <bogningrodney2@gmail.com>
2025-04-30 14:04:48 +0100

    file structure

---
+++ blocks/About/1/component.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='text-2xl text-center'>
+      Mechant test de la gloire 
+    </div>
+  )
+}
+
+export default About
